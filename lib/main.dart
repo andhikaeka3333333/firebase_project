84839cf51752ffdedc0ab35936239ddb92f8d12b
Realzzy <utubeperyone2@gmail.com>
2025-01-14 15:13:31 +0700

    feat: [NH] Add theme files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_project/controller/notif_controller.dart';
 import 'package:firebase_project/routes/myapp_route.dart';
+import 'package:firebase_project/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,7 +12,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await NotifController().initNotifications();
-  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Menyala Abangku',
       initialRoute: MyappRoute.login,
+      theme: AppTheme.getThemeData(),
       getPages: AppPages.pages,
     );
   }
